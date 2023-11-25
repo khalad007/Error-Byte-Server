ce386dc7a564cf14003404ac1f28a8313247508b
khalad007 <www.arifhossain49919@gmail.com>
2023-11-25 02:30:28 -0800

    Get operation for Show all class & Review

--- index.js
+++ index.js
@@ -10,40 +10,53 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcnyajy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const classesCollection = client.db("classDB").collection("classes");
+        const classesCollection = client.db("classDB").collection("classes");
+        const reviewCollection = client.db("classDB").collection("review");
 
-    // for popular classes (in home page)
-    app.get('/classes', async (req, res) => {
-        const result = await classesCollection.find().sort({ TotalEnrolment: -1 }).toArray();
-        res.send(result);
-    })
+        // for popular classes (in home page)
+        app.get('/classes', async (req, res) => {
+            const result = await classesCollection.find().sort({ TotalEnrolment: -1 }).toArray();
+            res.send(result);
+        })
+        // for review in home page 
+        app.get('/review', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result)
+        })
+        // for all classes 
+        app.get('/allClasses', async (req, res) => {
+            const result = await classesCollection.find().toArray();
+            res.send(result);
+        })
+
+      
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
