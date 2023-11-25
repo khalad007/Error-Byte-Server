f47cab8a0faa75577fefbcb49323b93989db8370
khalad007 <www.arifhossain49919@gmail.com>
2023-11-25 02:31:19 -0800

    Get operation for Single Class Details

--- index.js
+++ index.js
@@ -46,7 +46,20 @@ async function run() {
             res.send(result);
         })
 
-      
+        // for single class details 
+       
+        app.get('/allClasses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { Title: 1, Price: 1,Name: 1, Image: 1  },
+            };
+
+            const result = await classesCollection.findOne(query, options);
+            res.send(result);
+        })
 
 
 
